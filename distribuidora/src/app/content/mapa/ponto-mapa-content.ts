dc8a5253786bf28dd5b3d4548e65679932af5f87
Rafael Barcelos <rbarcelo@microsoft.com>
2017-04-30 17:16:16 -0700

    map content

--- distribuidora/src/app/content/mapa/ponto-mapa-content.ts
+++ distribuidora/src/app/content/mapa/ponto-mapa-content.ts
@@ -2,5 +2,12 @@ export class PontoMapaContent {
     public name:string;
     public endereco:string;
     public lastUpdated:Date;
-    public status:string;
+    public contentStatus:string;
+    public statusBar:IconedStatus[];
 }
+
+export class IconedStatus
+    {
+        public iconName:string;
+        public description:string;
+    }
